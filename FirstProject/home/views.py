05d5ee4f90945a290d964570582e52bea8ac7b16
Naman Markhedkar <77118629+marknaman05@users.noreply.github.com>
2023-05-26 22:18:02 +0530

    updated

---
+++ FirstProject/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def homepage(request):
+    return HttpResponse("<h2>This is my Home Page")
+
